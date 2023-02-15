b3de5619ca35c20af09b37ecae6576c3927bef73
Strram123 <121536281+Strram123@users.noreply.github.com>
2023-02-15 11:40:06 +0530

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -1,5 +1,5 @@
 
-
+var employe=[];
 const formEl=document.forms.Employeeform;
 console.log(formEl);
 const empNameEl=formEl.elements.empName;
@@ -18,10 +18,12 @@ const submit=(e)=>{
                          empid:id,
                          emppassword:password
                  };
-                 var employe=[];
-                 var jsons=JSON.stringify(empDetails);
-                 var local=localStorage.setItem("emp",employe+=jsons);
-                 JSON.parse(localStorage.getItem("emp"));
+                   var jsons=JSON.stringify(empDetails);
+                 console.log(employe);
+                 employe.push(jsons);
+                localStorage.jsons+=employe;
+                localStorage.setItem("Employee",employe);
+ //localStorage.Employees+=JSON.stringify(Employee)
                  window.open('file:///C:/Users/ELCOT/Desktop/HTMLPROGRAMS/employee1.html','_blank')   
 };
 formEl.addEventListener("submit",submit);
